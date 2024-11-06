4bcc51251a21d78c6f8e69db132dd9ba24b4d9eb
susan <susangithaiga26@gmail.com>
2024-11-06 06:49:17 +0300

    complete

---
+++ week-1-code-challenge/joke.py
@@ -0,0 +1,17 @@
+import random
+# store jokes in a variable
+my_jokes = [
+    "What’s the difference between ducks and dine-and-dashers? Ducks take care of their bills.",
+    "The reason dem yangu ni dentist? Ni coz ananifeel",
+    "If you got dumped by your partner while on an escaletor moving up, can we say that shit escalated quickly...?",
+    "Tangu nitext baddie `hey` aka reply `ni chakula ya farasi.` I just drink water and mind my business.",
+    "Guy laments about his cheating girlfriend to his friend. 2 days later they meet and his friend says. `nimeona ule manzi yako mjapanese` Mgani uyo? `Kanagawa Kumamoto`",
+    "If someone asks you why you're always blessed just tell them you know a Ngai.",
+    "What do you call two kikuyus who look alike? Doppleng'anga",
+    "`Mtu aniguze aone!` Jesus Christ, circa 23 BC.",
+    "This girl from Moscow won't let you hit it before you pay dowry, you have to Russia before you Putin",
+    "Ikipita watu watatu ni energy drink.",
+]
+
+random_joke = random.choice(my_jokes)
+print(random_joke)
